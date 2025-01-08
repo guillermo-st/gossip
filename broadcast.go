b330abdfdb44fe5a81d7e042358aded5f50272d1
Guillermo St <guillermo_stancanelli@outlook.com>
2025-01-07 22:32:47 -0300

    add timeout and buffer to stop blocking when sending message to slow subs

--- broadcast.go
+++ broadcast.go
@@ -2,35 +2,40 @@ package gossip
 
 import (
 	"errors"
+	"time"
 )
 
 var ErrBroadcasterClosed = errors.New("broadcaster is already closed and not accepting new subscribers or messages")
 
 type Broadcaster[T any] interface {
 	Subscribe() (chan T, error)
-	Unsubscribe(chan<- T)
+	Unsubscribe(chan T)
 	Broadcast(T) error
 	Close()
 }
 
 type DefaultBroadcaster[T any] struct {
-	subscribers map[chan<- T]struct{}
-	subStream   chan chan<- T
-	unSubStream chan chan<- T
-	pubStream   chan T
-	done        chan struct{}
-	doneAck     chan struct{}
+	subscribers    map[chan<- T]struct{}
+	subStream      chan chan<- T
+	unSubStream    chan chan T
+	pubStream      chan T
+	done           chan struct{}
+	doneAck        chan struct{}
+	subSendTimeout time.Duration
+	subBufLen      int
 }
 
 // NewBroadcaster returns a new instance of the default Broadcaster implementation with the given input buffer length.
-func NewBroadcaster[T any](bufLen int) *DefaultBroadcaster[T] {
+func NewBroadcaster[T any](broadcastBufLen, subBufLen int, subSendTimeout time.Duration) *DefaultBroadcaster[T] {
 	b := &DefaultBroadcaster[T]{
-		subscribers: make(map[chan<- T]struct{}),
-		subStream:   make(chan chan<- T),
-		unSubStream: make(chan chan<- T),
-		pubStream:   make(chan T, bufLen),
-		done:        make(chan struct{}),
-		doneAck:     make(chan struct{}),
+		subscribers:    make(map[chan<- T]struct{}),
+		subStream:      make(chan chan<- T),
+		unSubStream:    make(chan chan T),
+		pubStream:      make(chan T, broadcastBufLen),
+		done:           make(chan struct{}),
+		doneAck:        make(chan struct{}),
+		subBufLen:      subBufLen,
+		subSendTimeout: subSendTimeout,
 	}
 
 	go b.broadcast()
@@ -43,7 +48,7 @@ Subscribe adds a new subscriber to the broadcaster. All subscribers will receive
 The returned channel shouldn't be closed by the client to prevent panics. Instead, the client should call Unsubscribe() on that channel to remove the subscriber from the broadcaster.
 */
 func (b *DefaultBroadcaster[T]) Subscribe() (chan T, error) {
-	sub := make(chan T)
+	sub := make(chan T, b.subBufLen)
 	select {
 	case b.subStream <- sub:
 		return sub, nil
@@ -54,7 +59,7 @@ func (b *DefaultBroadcaster[T]) Subscribe() (chan T, error) {
 }
 
 // Unsubscribe removes a subscriber (that is, the channel returned by Subscribe()) from the broadcaster so that it no longer receives messages.
-func (b *DefaultBroadcaster[T]) Unsubscribe(sub chan<- T) {
+func (b *DefaultBroadcaster[T]) Unsubscribe(sub chan T) {
 	select {
 	case b.unSubStream <- sub:
 	case <-b.done:
@@ -106,7 +111,11 @@ func (b *DefaultBroadcaster[T]) broadcast() {
 
 		case msg := <-b.pubStream:
 			for sub := range b.subscribers {
-				sub <- msg
+				select {
+				case sub <- msg:
+				case <-time.After(b.subSendTimeout):
+					continue
+				}
 			}
 		}
 	}
